1748467df44120e5b851068dc8a0ca53d86dd5b2
John Kosh <john.kosh@greatdayimprovements.com>
2023-11-22 11:07:51 -0500

    updated to .net8

--- SqliteConsole.Infrastructure/Data/TestContext.cs
+++ SqliteConsole.Infrastructure/Data/TestContext.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using SqliteConsole.Infrastructure.Models;
 
-namespace SqliteConsole.Infrastructure.Models;
+namespace SqliteConsole.Infrastructure.Data;
 
 public partial class TestContext : DbContext
 {
